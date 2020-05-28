07cde0a56bd51a42ce26829e5647f77dcf7a0566
Jim Olsen <jim.olsen@modelop.com>
2020-05-28 09:19:45 -0600

    Initial Commit

---
+++ splitter-py.py
@@ -0,0 +1,8 @@
+# fastscore.schema.0: array-double
+# fastscore.schema.1: array-double
+# fastscore.schema.3: array-double
+
+# modelop.score
+def action(datum):
+    yield (1, datum)
+    yield (3, datum)
